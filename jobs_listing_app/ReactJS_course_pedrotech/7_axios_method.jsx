c6b633cf63c6623a859e0bfbc181a1febb51532f
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-07 15:51:32 +0100

    refactoring codes usinfg axios to fetch data from api

--- jobs_listing_app/ReactJS_course_pedrotech/7_axios_method.jsx
+++ jobs_listing_app/ReactJS_course_pedrotech/7_axios_method.jsx
@@ -9,16 +9,20 @@ import Axios from "axios"
 function App() {
   const [catfact, setCatFact] = useState("");
 // Using use effect to prevent an infinite loop
-  useEffect(() => {
-    Axios.get("https://catfact.ninja/fact").then((resp) => {
+
+const fxn_catfact = () => {
+  Axios.get("https://catfact.ninja/fact").then((resp) => {
     setCatFact(resp.data.fact);
   });
+};
+  useEffect(() => {
+    fxn_catfact();
   }, []);
 
   
   return (
     <div className="App">
-      <button> GENERATE CAT FACT </button>
+      <button onClick={fxn_catfact}> FUN FACTs - CATs EDITION </button>
       <p> {catfact} </p>
     </div>
   )
